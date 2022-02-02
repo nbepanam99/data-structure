c8dd114b77c35b9b6da82b67619f105a49ce8a66
nbepanam99 <nbepanam@gmail.com>
2022-02-02 10:05:42 +0100

    Bubble Sort

--- src/test/java/Algorithm.java
+++ src/test/java/Algorithm.java
@@ -7,7 +7,6 @@ public class Algorithm {
 
     @Test
     void shouldUseBubbleSort() {
-
         int[] array = {6,2,3,4,1,5};
         System.out.println(Arrays.toString(array));
         for (int i=0; i< array.length - 1; i++){
@@ -20,5 +19,26 @@ public class Algorithm {
             }
         }
         System.out.println(Arrays.toString(array));
+        int[] array2 = {6,2,3,4,1,5};
+        Arrays.sort(array2);
+        System.out.println(Arrays.toString(array2));
+    }
+
+    @Test
+    void shouldUseSelectionSort() {
+        int[] array = {6,2,3,4,1,5};
+        for (var i=0; i <array.length;i++){
+            for (var j = 1; j<array.length-1-i; j++){
+                if (array[i] > array[j]){
+                    var temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                }
+            }
+
+        }
+
+        System.out.println(Arrays.toString(array));
+
     }
 }
