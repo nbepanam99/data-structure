f5a25f20a64a43eadcbbab0ce338d45d7ca89efa
nbepanam99 <nbepanam@gmail.com>
2023-01-02 11:40:31 +0100

    add prototype pattern 2

--- src/main/java/design_patterns/creational/prototype/shapes/Shape.java
+++ src/main/java/design_patterns/creational/prototype/shapes/Shape.java
@@ -1,2 +1,43 @@
-package design_patterns.creational.prototype.shapes;public class Shape {
+package design_patterns.creational.prototype.shapes;
+
+import java.util.Objects;
+
+public abstract class Shape {
+
+    public int x;
+    public int y;
+    public String color;
+
+    public Shape() {
+    }
+
+
+    public Shape(int x, int y, String color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public Shape(Shape target) {
+        if (target != null) {
+            this.x = target.x;
+            this.y = target.y;
+            this.color = target.color;
+        }
+    }
+
+    public abstract Shape clone();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Shape shape = (Shape) o;
+        return x == shape.x && y == shape.y && Objects.equals(color, shape.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, color);
+    }
 }
